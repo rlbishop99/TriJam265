565108301b77daf5e099cf33a339646790b0d502
Rocko Bishop <58998967+rlbishop99@users.noreply.github.com>
2024-04-12 19:40:31 -0500

    Updated folder directory
    Added new folders for organization, added temp face assets for different viruses, and created a 2d controller with bounds.

---
+++ Micro Scope/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private float x;
+    private float y;
+    private float xmin = -10f;
+    private float xmax = 10f;
+    private float ymin = -10f;
+    private float ymax = 10f;
+
+    private float xPos;
+    private float yPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xPos = transform.position.x;
+        yPos = transform.position.y;
+        CheckBounds(xPos, yPos);
+
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(x, y, 0);
+
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
+
+    void CheckBounds(float x, float y) {
+
+        if(x < xmin){
+
+            if(y > ymax){
+                transform.position = new Vector3(xmin, ymax, -10);
+            }
+            else{
+                transform.position = new Vector3(xmin, y, -10);
+            }
+        }
+        
+        if(x > xmax){
+
+            if(y > ymax) {
+                transform.position = new Vector3(xmax, ymax, -10);
+            }
+            else {
+                transform.position = new Vector3(xmax, y, -10);
+            }
+        }
+        
+        if(y < ymin){
+
+            if(x > xmax){
+                transform.position = new Vector3(xmax, ymin, -10);
+            } else if(x < xmin){
+                transform.position = new Vector3(xmin, ymin, -10);
+            } else{
+                transform.position = new Vector3(x, ymin, -10);
+            }
+        }
+        
+        if(y > ymax){
+
+            if(x < xmin){
+                transform.position = new Vector3(xmin, ymax, -10);
+            } else if(x > xmax) {
+                transform.position = new Vector3(xmax, ymax, -10);
+            } else{
+                transform.position = new Vector3(x, ymax, -10);
+            }
+        }
+    }
+}
